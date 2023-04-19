9377e1ed5b125b3a4e9664efe0fdee2840d1b8aa
JudyChan <tina19640@gmail.com>
2023-04-18 22:43:44 -0500

    refine the structure

--- simulation.py
+++ simulation.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import random
-from typing import List
+from typing import List, Tuple
 
 import pandas as pd
 
@@ -57,31 +57,6 @@ class Team:
     batters: List[Batter]
 
 
-def simulate_game(team1: Team, team2: Team, pitch_count: int) -> tuple[float, float]:
-
-    # TO-DO
-    team1_win_rate = 0
-    team2_win_rate = 0
-
-    return team1_win_rate, team2_win_rate
-
-
-def monte_carlo_simulation(team1: Team, team2: Team, num_iterations: int, pitch_count: int) -> tuple[float, float]:
-    team1_wins = 0
-    team2_wins = 0
-
-    for i in range(num_iterations):
-        team1_win_rate, team2_win_rate = simulate_game(team1, team2, pitch_count)
-        if team1_win_rate > team2_win_rate:
-            team1_wins += 1
-        else:
-            team2_wins += 1
-
-    team1_win_rate = team1_wins / num_iterations
-    team2_win_rate = team2_wins / num_iterations
-
-    return team1_win_rate, team2_win_rate
-
 def generate_players(player_type, country, num_of_players):
     if player_type not in ['Pitcher', 'Batter']:
         raise ValueError("Invalid player type. Choose either 'Pitcher' or 'Batter'.")
@@ -117,6 +92,74 @@ def generate_team(country, num_pitchers, num_batters):
     return Team(pitchers, batters)
 
 
+def pitching_score(pitchers: List[Pitcher], pitch_count: int) -> float:
+    # To-Do 投手的最低用球數？可以只投一顆嗎？
+    p1 = random.randint(10, pitch_count-10)
+    p2 = random.randint(10, pitch_count-10-p1)
+    p3 = pitch_count - p1 - p2
+
+    pitch_for_each = [p1, p2, p3]
+
+    # 計算每位投手的用球數百分比
+    pitch_count_percentage = [count / pitch_count for count in pitch_for_each]
+
+    # 計算每位投手的加權表現
+    # TO-DO 公式
+    weighted_performance = [percentage * pitcher.era_minus_xera_diff for percentage, pitcher in
+                            zip(pitch_count_percentage, pitchers)]
+
+    print(pitch_for_each)
+    #return total_pitching_score
+
+
+def hitting_score(batters: List[Batter]) -> float:
+    total_hitting_score = sum([batter.brl_percent for batter in batters])
+
+    return total_hitting_score
+
+
+def normalize_scores(score1: float, score2: float) -> Tuple[float, float]:
+    total_score = score1 + score2
+    return score1 / total_score, score2 / total_score
+
+
+def calculate_win_rate(team1: Team, team2: Team, pitch_count: int) -> Tuple[float, float]:
+    team1_pitching_score = pitching_score(team1.pitchers, pitch_count)
+    team1_hitting_score = hitting_score(team1.batters)
+    team1_total_score = team1_pitching_score + team1_hitting_score
+    print(pitch_count, team1_pitching_score, team1_hitting_score)
+    print(' ')
+
+    team2_pitching_score = pitching_score(team2.pitchers, pitch_count)
+    team2_hitting_score = hitting_score(team2.batters)
+    team2_total_score = team2_pitching_score + team2_hitting_score
+    print(pitch_count, team2_pitching_score, team2_hitting_score)
+    print('------')
+
+    team1_win_rate, team2_win_rate = normalize_scores(team1_total_score, team2_total_score)
+
+    return team1_win_rate, team2_win_rate
+
+
+def monte_carlo_simulation(team1: Team, team2: Team, num_iterations: int) -> tuple[float, float]:
+    team1_wins = 0
+    team2_wins = 0
+
+    for i in range(num_iterations):
+        pitch_count = random.randint(300, 500)
+        team1_win_rate, team2_win_rate = calculate_win_rate(team1, team2, pitch_count)
+        if team1_win_rate > team2_win_rate:
+            team1_wins += 1
+        else:
+            team2_wins += 1
+
+    team1_win_rate = team1_wins / num_iterations
+    team2_win_rate = team2_wins / num_iterations
+
+    return team1_win_rate, team2_win_rate
+
+
+
 if __name__ == "__main__":
 
     countries = ["Australia", "Cuba", "Italy", "Japan", "Mexico", "Puerto Rico", "USA", "Venezuela"]
@@ -134,8 +177,8 @@ if __name__ == "__main__":
     team1 = generate_team(countryA, 3, 9)
     team2 = generate_team(countryB, 3, 9)
 
-    team1_win_rate, team2_win_rate = monte_carlo_simulation(team1, team2, num_iterations=10000,
-                                                            pitch_count=random.randint(50, 100))
+    # To-Do: Pitch Count 可以改成使用者輸入
+    team1_win_rate, team2_win_rate = monte_carlo_simulation(team1, team2, num_iterations=10)
 
     print(f"Team 1 ({team1.pitchers[0].Country}) win rate: {team1_win_rate}")
     print(f"Team 2 ({team2.pitchers[0].Country}) win rate: {team2_win_rate}")
