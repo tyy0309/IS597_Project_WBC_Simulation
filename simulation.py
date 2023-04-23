0e51228c169971f1406dbfef7a149ea8a7a09006
JudyChan <tina19640@gmail.com>
2023-04-23 13:30:38 -0500

    modify summary - conditional probability

--- simulation.py
+++ simulation.py
@@ -203,11 +203,11 @@ def hitting_score(batters: List[Batter]) -> float:
 def calculate_total_score(team1: Team, team2: Team, pitch_count: int):
     p1_cnt, p1_cnt_pct, team1_pitching_score = pitching_score(team1.pitchers, pitch_count)
     team1_hitting_score = hitting_score(team1.batters)
-    team1_total_score = (0.7 * team1_pitching_score) + (0.3 * team1_hitting_score)
+    team1_total_score = (0.8 * team1_pitching_score) + (0.2 * team1_hitting_score)
 
     p2_cnt, p2_cnt_pct, team2_pitching_score = pitching_score(team2.pitchers, pitch_count)
     team2_hitting_score = hitting_score(team2.batters)
-    team2_total_score = (0.7 * team2_pitching_score) + (0.3 * team2_hitting_score)
+    team2_total_score = (0.8 * team2_pitching_score) + (0.2 * team2_hitting_score)
 
     return team1_total_score, team2_total_score, p1_cnt, p2_cnt
 
@@ -215,63 +215,48 @@ def calculate_total_score(team1: Team, team2: Team, pitch_count: int):
 def monte_carlo_simulation(team1: Team, team2: Team, num_iterations: int) -> tuple[int, int, float, float]:
     team1_wins = 0
     team2_wins = 0
-    p1_cnt_sum = 0
-    p2_cnt_sum = 0
-    p1_cnt_pct_sum = 0
-    p2_cnt_pct_sum = 0
-    p1_cnt_pct_list = []
-    team1_results = []
-    team2_results = []
-
+    team1_more_pitches_when_winning = 0
+    team2_more_pitches_when_winning = 0
 
     print(f'\n{"sim":<10}{"t1-Country":<15}{"t1-p1_cnt":<15}{"t1-p1_cnt_%":<15}{"t1-result":<15}{"|":<5}{"t2-Country":<15}{"t2-p1_cnt":<15}{"t2-p1_cnt_%":<15}{"t2-result":<15}')
 
     for i in range(num_iterations):
         random.seed()  # Reset the random seed for each iteration
-
-        # 一場比賽共有 27 個出局數，平均每局面對打者的球數通常在 15-20 之間（面對每名打者投球次數 3.5-4 之間，每局平均面對 4 名打者）
-        # 估計一場比賽所有投手可能總共需投出約 120-200 顆球
         pitch_count = random.randint(120, 200)
 
         team1_total_score, team2_total_score, p1_cnt, p2_cnt = calculate_total_score(team1, team2, pitch_count)
-        p1_cnt_sum += p1_cnt
-        p2_cnt_sum += p2_cnt
         p1_cnt_pct = p1_cnt / pitch_count
         p2_cnt_pct = p2_cnt / pitch_count
-        p1_cnt_pct_sum += p1_cnt_pct
-        p2_cnt_pct_sum += p2_cnt_pct
-        p1_cnt_pct_list.append(p1_cnt_pct)
-        team1_results.append(team1_total_score)
-        team2_results.append(team2_total_score)
 
         if team1_total_score > team2_total_score:
             team1_wins += 1
             result = 'win'
+            if p1_cnt > p2_cnt:
+                team1_more_pitches_when_winning += 1
         else:
             team2_wins += 1
             result = 'lose'
+            if p2_cnt > p1_cnt:
+                team2_more_pitches_when_winning += 1
 
-        print(f'{(i + 1):<10}{team1.pitchers[0].country:<15}{p1_cnt:<15}{round(p1_cnt_pct, 2):<15}{result:<15}{"|":<5}{team2.pitchers[0].country:<15}{p2_cnt:<15}{round(p2_cnt_pct, 2):<15}{"win" if result == "lose" else "lose":<15}')
+        print(
+            f'{(i + 1):<10}{team1.pitchers[0].country:<15}{p1_cnt:<15}{round(p1_cnt_pct, 2):<15}{result:<15}{"|":<5}{team2.pitchers[0].country:<15}{p2_cnt:<15}{round(p2_cnt_pct, 2):<15}{"win" if result == "lose" else "lose":<15}')
 
     team1_win_rate = team1_wins / num_iterations
     team2_win_rate = team2_wins / num_iterations
 
-    p1_avg_cnt = p1_cnt_sum / num_iterations
-    p2_avg_cnt = p2_cnt_sum / num_iterations
-
-    p1_avg_cnt_pct = p1_cnt_pct_sum / num_iterations
-    p2_avg_cnt_pct = p2_cnt_pct_sum / num_iterations
+    # Calculate conditional probabilities
+    p_A_given_B = team1_more_pitches_when_winning / team1_wins
+    p_C_given_D = team2_more_pitches_when_winning / team2_wins
 
     print("\nSummary Statistics")
     print("simulation times:", num_iterations)
-    print(f'{"Team 1":<20}{" ":<10}{" ":<10}{" ":<10}{" ":<15}{" ":<20}{"Team 2":<20}')
-    print(
-        f'{"avg_1st_p_count":<20}{"avg_1st_p_cnt_%":<20}{"win_times":<15}{"lose_times":<15}{"win_rate":<15}{"avg_1st_p_count":<20}{"avg_1st_p_cnt_%":<20}{"win_times":<15}{"lose_times":<15}{"win_rate":<15}')
-    print(
-        f'{round(p1_avg_cnt, 2):<20}{round(p1_avg_cnt_pct, 2):<20}{team1_wins:<15}{team2_wins:<15}{round(team1_win_rate, 3):<15}{round(p2_avg_cnt, 2):<20}{round(p2_avg_cnt_pct, 2):<20}{team2_wins:<15}{team1_wins:<15}{round(team2_win_rate, 3):<15}')
+    print(f'{"Team 1":<20}{" ":<10}{" ":<10}{" ":<15}{"Team 2":<20}')
+    print(f'{"P(A|B)":<10}{"win_times":<15}{"lose_times":<15}{"win_rate/P(B)":<15}{"P(C|D)":<10}{"win_times":<15}{"lose_times":<15}{"win_rate/P(D)":<15}')
+    print(f'{round(p_A_given_B, 2):<10}{team1_wins:<15}{team2_wins:<15}{round(team1_win_rate, 2):<15}{round(p_C_given_D, 2):<10}{team2_wins:<15}{team1_wins:<15}{round(team2_win_rate, 2):<15}')
 
-    # return team1_wins, team2_wins, team1_win_rate, team2_win_rate
-    return team1_wins, team2_wins, team1_win_rate, team2_win_rate, p1_cnt_pct_list, team1_results, team2_results
+    return team1_wins, team2_wins, team1_win_rate, team2_win_rate
+
 
 
 # Creating a plot to validate monte_carlo_simulation function
@@ -309,5 +294,5 @@ if __name__ == "__main__":
     # print(team1.batters)
     # print(pitching_score(team1.pitchers, 300))
     # print(hitting_score(team1.batters))
-    team1_wins, team2_wins, team1_win_rate, team2_win_rate, p1_cnt_pct_list, team1_results, team2_results = monte_carlo_simulation(team1, team2, num_iterations)
-    create_plot(team1, team2, p1_cnt_pct_list, team1_results, team2_results)
+    team1_wins, team2_wins, team1_win_rate, team2_win_rate = monte_carlo_simulation(team1, team2, num_iterations)
+    # create_plot(team1, team2, p1_cnt_pct_list, team1_results, team2_results)
